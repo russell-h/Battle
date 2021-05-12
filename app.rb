28fed99d5bb803bf4c03458e530b59e5d33e3de2
Russell Horth <russell.horth@gmail.com>
2021-05-12 21:48:19 +0100

    app.rb updated with ..

--- app.rb
+++ app.rb
@@ -27,20 +27,20 @@ class Battle < Sinatra::Base
   
 
   post '/names' do
-    session[:player_1_name] = params[:player_1_name]
-    session[:player_2_name] = params[:player_2_name]
+    $player_1 = PLayer.new(params[:player_1_name])
+    $player_2 = Player.new(params[:player_2_name])
     redirect '/play'
   end
   
 get '/play' do 
-  @player_1_name = session[:player_1_name]
-  @player_2_name = session[:player_2_name]
+  @player_1_name = $player_1
+  @player_2_name = $player2
   erb :play
 end
 
 get '/attack' do
-  @player_1_name = session[:player_1_name]
-  @player_2_name = session[:player_2_name]
+  @player_1_name = $player_1
+  @player_2_name = $player_2
   erb :attack
 end
   
