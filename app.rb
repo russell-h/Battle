d9a0d5c33b8b69064c62cff99619a7d845dbbe8b
Russell Horth <russell.horth@gmail.com>
2021-05-12 21:56:57 +0100

    tests pass

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
-require 'Player'
+require 'player'
 
 class Battle < Sinatra::Base
   configure :development do 
@@ -27,20 +27,20 @@ class Battle < Sinatra::Base
   
 
   post '/names' do
-    $player_1 = PLayer.new(params[:player_1_name])
+    $player_1 = Player.new(params[:player_1_name])
     $player_2 = Player.new(params[:player_2_name])
     redirect '/play'
   end
   
 get '/play' do 
-  @player_1_name = $player_1
-  @player_2_name = $player2
+  @player_1_name = $player_1.name
+  @player_2_name = $player_2.name
   erb :play
 end
 
 get '/attack' do
-  @player_1_name = $player_1
-  @player_2_name = $player_2
+  @player_1_name = $player_1.name
+  @player_2_name = $player_2.name
   erb :attack
 end
   
