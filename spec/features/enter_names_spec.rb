a3700b4d58bdcce9ce3144a920fd1056f8991424
Russell Horth <russell.horth@gmail.com>
2021-05-12 15:43:53 +0100

    Battle setup

---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,12 @@
+feature 'Enter names' do 
+  scenario 'submitting names' do
+    visit('/')
+    fill_in :player_1_name, with: 'Charlotte'
+    fill_in :player_2_name, with: 'Mittens'
+    click_button 'Submit'
+
+    save_and_open_page
+    
+    expect(page).to have_content 'Charlotte vs. Mittens'
+  end
+end
